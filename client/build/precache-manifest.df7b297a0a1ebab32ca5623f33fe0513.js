b72803aeaf2ee85ae70fef64a93c86a93f8dd3d7
Adrian Pearman <adrianpearman12@gmail.com>
2020-05-28 21:36:47 -0400

    adding client build.

---
+++ client/build/precache-manifest.df7b297a0a1ebab32ca5623f33fe0513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2b53f1168abdcc21842417e9c27771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efbd7ba76c57893bf910",
+    "url": "/static/css/main.826533ad.chunk.css"
+  },
+  {
+    "revision": "f7e9da74cfe37b061155",
+    "url": "/static/js/2.8bc765fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8bc765fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efbd7ba76c57893bf910",
+    "url": "/static/js/main.764aa638.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
